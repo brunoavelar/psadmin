050608e7310856e366e1d18c31f354c97e0c0f67
Bruno Avelar <brunoavelar@gmail.com>
2016-01-15 17:01:46 -0300

    Add pages and routing

--- gulpfile.js
+++ gulpfile.js
@@ -67,7 +67,7 @@ gulp.task('css', function() {
 
 gulp.task('lint', function() {
     return gulp.src(config.paths.js)
-        .pipe(lint())
+        .pipe(lint({config: 'eslint.config.json'}))
         .pipe(lint.format());
 });
 
