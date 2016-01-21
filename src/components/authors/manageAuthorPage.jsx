d61def97570056f91d3960e44559078d29c6ce71
Bruno Avelar <brunoavelar@gmail.com>
2016-01-21 17:45:03 -0300

    Add save author

--- src/components/authors/manageAuthorPage.jsx
+++ src/components/authors/manageAuthorPage.jsx
@@ -1,5 +1,6 @@
 var React = require('react');
 var AuthorForm = require('./authorForm.jsx');
+var AuthorApi = require('../../api/authorApi')
 
 var manageAuthorPage = React.createClass({
 	displayName: "manage Author Page",
@@ -11,16 +12,22 @@ var manageAuthorPage = React.createClass({
 	setAuthorState: function(event){
 		var field = event.target.name;
 		var value = event.target.value;
-		this.setState({ "author[field]": value })
-
+        this.state.author[field] = value;
 
 		return this.setState({ author: this.state.author });
 	},
+
+    saveAuthor: function(event){
+        event.preventDefault();
+        AuthorApi.saveAuthor(this.state.author);
+    },
+
 	render: function() {
 		return (
 			<AuthorForm
                 author={this.state.author}
                 onChange={this.setAuthorState}
+                onSave={this.saveAuthor}
 			/>
 			);
 	}
